d1bd6e4d3f4c157237cfe0d12b3b3b0dac494735
jini1234 <jinishat17@gmail.com>
2022-06-18 17:06:57 +0530

    Implement Package cost calculation and setting discount

--- src/main/java/com/everest/delivery/Package.java
+++ src/main/java/com/everest/delivery/Package.java
@@ -3,18 +3,36 @@ package com.everest.delivery;
 import com.everest.delivery.offer.Offer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
 public class Package {
-    private int id, weightKgs, distanceKms;
+    private String id;
+    private int weightKgs, distanceKms;
+    @Setter
+    private double cost, discount;
     private Offer offer;
 
-    public double calculateCost(int baseDeliveryCost){
-        return 0.0;
+    public Package(String id, int weightKgs, int distanceKms, Offer offer) {
+        this.id = id;
+        this.weightKgs = weightKgs;
+        this.distanceKms = distanceKms;
+        this.offer = offer;
     }
 
-    private boolean isOfferApplicable(){
-        return false;
+    public void calculateCost(int baseDeliveryCost) {
+        double cost = baseDeliveryCost + (this.weightKgs * 10) + (this.distanceKms * 5);
+        if (isOfferApplicableForPackage()) {
+            double discount = cost * (offer.getDiscountPercent() / 100);
+            cost -= discount;
+            this.setDiscount(discount);
+        }
+        this.setCost(cost);
+
+    }
+
+    private boolean isOfferApplicableForPackage() {
+        return this.offer.isApplicable(this.distanceKms, this.weightKgs);
     }
 }
