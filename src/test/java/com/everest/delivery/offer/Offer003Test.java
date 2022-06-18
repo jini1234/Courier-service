4b39878df6aad2e9591a050613e5152d92d083cd
jini1234 <jinishat17@gmail.com>
2022-06-18 15:53:27 +0530

    Test for Offer3 checking discount applicability

--- src/test/java/com/everest/delivery/offer/Offer003Test.java
+++ src/test/java/com/everest/delivery/offer/Offer003Test.java
@@ -1,8 +1,10 @@
 package com.everest.delivery.offer;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class Offer003Test {
@@ -12,5 +14,19 @@ public class Offer003Test {
         Offer ofr003 = new Offer003();
         assertEquals("OFR003", ofr003.getId());
         assertEquals(5.0, ofr003.getDiscountPercent(), 0.0);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"50,10", "250,150", "51,11", "249,149"})
+    public void should_match_applicable_true(int distance, int weight) {
+        Offer ofr003 = new Offer003();
+        assertTrue(ofr003.isApplicable(distance, weight));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"49,9", "251,151", "50,200", "49,5"})
+    public void should_match_applicable_false(int distance, int weight) {
+        Offer ofr003 = new Offer003();
+        assertFalse(ofr003.isApplicable(distance, weight));
     }
 }
